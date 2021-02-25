03428628fa7c9cb3c2dbd0ee79592f1bc925a43d
GiriNeko <king@ineko.cc>
2021-02-25 15:46:43 +0800

    fix some bugs

--- 17wanxiao.py
+++ 17wanxiao.py
@@ -272,18 +272,17 @@ def check_in(username, password,deviceId,failtime):
 def server_push(sckey, desp):
     send_url = f"https://sctapi.ftqq.com/{sckey}.send"
     params = {
-        "text": "健康打卡推送通知",
+        "title": "健康打卡推送通知",
         "desp": desp
     }
     # 发送消息
     res = requests.post(send_url, data=params)
     # {"errno":0,"errmsg":"success","dataset":"done"}
-    # logging.info(res.text)
+    logging.info(res.text)
     try:
-        if not res.json()['errno']:
+        if not res.json()['error']=="SUCCESS":
             logging.info('Server酱推送服务成功')
-        else:
-            logging.warning('Server酱推送服务失败')
+
     except:
         logging.warning("Server酱不起作用了，可能是你的sckey出现了问题")
 
